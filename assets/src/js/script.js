4a0f455fefa3add5ace34ebedb542e8da27c209d
Josemar <josemar21@outlook.pt>
2026-02-18 19:00:55 +0100

    Terminei a v1 do meu menu

--- assets/src/js/script.js
+++ assets/src/js/script.js
@@ -1,3 +1,17 @@
+var menu = document.getElementById('menu')
+var menuContainer = document.getElementById('menu-container')
+
+menu.addEventListener('click', mostrarMenu)
+
+
+function mostrarMenu() {
+    if (menuContainer.classList.contains('hidden')) {
+        menuContainer.classList.remove('hidden')
+        return
+    } 
+    menuContainer.classList.add('hidden')
+}
+
 function atualizarAno() {
     let anoActual = new Date().getFullYear()   
 
