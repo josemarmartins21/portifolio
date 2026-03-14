cb0f6a5a33ee162a63e06605fffde81771034cbd
Josemar <josemar21@outlook.pt>
2026-03-14 19:30:59 +0100

    Terminei a refatoração

--- assets/src/js/script.js
+++ assets/src/js/script.js
@@ -1,9 +1,15 @@
 var menu = document.getElementById('menu')
 var menuContainer = document.getElementById('menu-container')
+var janela = window
 
+
+janela.addEventListener('scroll', esconderMenuNoScroll)
 menu.addEventListener('click', mostrarMenu)
 
-
+/**
+ * Mostrar e fechar Menu ao clicar.
+ * @returns Mostra
+ */
 function mostrarMenu() {
     if (menuContainer.classList.contains('hidden')) {
         menuContainer.classList.remove('hidden')
@@ -12,10 +18,30 @@ function mostrarMenu() {
     menuContainer.classList.add('hidden')
 }
 
+/*
+* Esconder o menu ao scrolar 
+*/
+function esconderMenuNoScroll() {
+    if (! menuContainer.classList.contains('hidden')) {
+        menuContainer.classList.add('hidden')
+    }
+}
+
+/**
+ * Atualizar o Ano do Footer
+ */
 function atualizarAno() {
     let anoActual = new Date().getFullYear()   
-
     let ano = document.getElementById('ano')
     
     ano.innerText = `${anoActual}`
+
+}
+
+/**
+ * Debugador
+ */
+function dd(v) {
+    console.log(v)
+    
 }
---
+++ content.md
@@ -0,0 +1,7 @@
+Estou a cursar o último ano do curso técnico médio de informática no Colégio Mundo Novo, onde adquiri conhecimentos sólidos em diversas áreas da informática, incluindo programação, redes de computadores, entre outros.
+
+
+
+## Experiencia
+
+Atuo como técnico de redes de computadores na QoS Tel, onde sou responsável pela instalação, configuração e manutenção de redes em ambientes residenciais e empresariais. Adquiri experiência prática na resolução de problemas de conectividade e suporte ao cliente.
